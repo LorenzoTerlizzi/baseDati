e9eefbca9a0fb7f13908c1e6de0fd17a3447016f
LorenzoTerlizzi <lorenzoterlizzi2004@gmail.com>
2024-09-25 09:29:01 +0200

    nuovo

--- EsercitazioniVisualStudio/accademia/query_accademia5.sql
+++ EsercitazioniVisualStudio/accademia/query_accademia5.sql
@@ -1,5 +1,5 @@
 --1--
-select distinct WP.nome, WP.inizio, WP.fine
+select WP.nome, WP.inizio, WP.fine
 from WP, Progetto
 where WP.progetto = Progetto.id
 and Progetto.nome = 'Pegasus';
@@ -19,6 +19,15 @@ and Progetto.nome = 'Pegasus'
 group by Persona.nome, Persona.cognome, Persona.posizione
 having count(AttivitaProgetto.id) > 1;
 
+--3 alternativa senza usare having count()--
+select distinct s.id, s.nome, s.cognome, s.posizione
+from AttivitaProgetto a1, AttivitaProgetto A2, Persona s, Progetto persona
+where a1.id, <> a2.id
+and a1.progetto = a2.progetto
+and a1.persona = a2.persona
+and a1.persona = s.id
+and a1.persona = p.id
+and p.nome = 'Pegasus'  
 
 --4--
 select distinct Persona.nome, Persona.cognome, Persona.posizione
@@ -34,22 +43,41 @@ where Persona.id = Assenza.persona
 and Persona.posizione = 'Professore Ordinario' and Assenza.tipo = 'Malattia'
 group by Persona.nome, Persona.cognome, Persona.posizione
 having count(Assenza.id) > 1;
+--5 alternativa senza usare having count()--
+select *
+from persona s, assenza a1, assenza a2
+where a1.persona = s.id
+and a2.persona = s.id
+and a1.id <> a2.id
+and a1.tipo = 'Malattia'
+and a2.tipo = 'Malattia'
+and s.posizione = 'Professore Ordinario';
 
 
 --6--
 select distinct Persona.nome, Persona.cognome, Persona.posizione
 from Persona, AttivitaNonProgettuale
 where Persona.id = AttivitaNonProgettuale.persona
-and Persona.posizione = 'Ricercatore' and AttivitaNonProgettuale.tipo = 'Didattica';
+and Persona.posizione = 'Ricercatore'
+and AttivitaNonProgettuale.tipo = 'Didattica';
 
 
 --7--
 select Persona.nome, Persona.cognome, Persona.posizione
-from Persona, AttivitaNonProgettuale
+from Persona, AttivitaNonProgettuale a1, AttivitaNonProgettuale a2
 where Persona.id = AttivitaNonProgettuale.persona
 and Persona.posizione = 'Ricercatore' and AttivitaNonProgettuale.tipo = 'Didattica'
 group by Persona.nome, Persona.cognome, Persona.posizione
 having count(AttivitaNonProgettuale.id) > 1;
+-- 7--
+select distinct p.nome, p.cognome, p.posizione
+from persona p, AttivitaNonProgettuale an1, AttivitaNonProgettuale an2
+where an1.id <> an2.id
+and an1.tipo = 'Didattica'
+and an2.tipo = 'Didattica'
+and an1.persona = p.id
+and an2 = p.id
+and p.posizione = 'Ricercatore';
 
 
 --8--
@@ -60,36 +88,38 @@ and AttivitaProgetto.giorno = AttivitaNonProgettuale.giorno;
 
 
 --9--
-select distinct Persona.nome, Persona.cognome, AttivitaProgetto.giorno, Progetto.nome,
-       AttivitaNonProgettuale.tipo, AttivitaProgetto.oreDurata oreProg, AttivitaNonProgettuale.oreDurata oreNonProg
-from Persona, AttivitaProgetto, AttivitaNonProgettuale, Progetto
-where Persona.id = AttivitaProgetto.persona
-and Persona.id = AttivitaNonProgettuale.persona
-and AttivitaProgetto.progetto = Progetto.id
-and AttivitaProgetto.giorno = AttivitaNonProgettuale.giorno;
+select s.nome, s.cognome, ap.giorno, p.nome,
+       anp.tipo, ap.oreDurata oreProg, anp.oreDurata oreNonProg
+from Persona s, AttivitaProgetto ap, AttivitaNonProgettuale anp, Progetto p
+where s.id = ap.persona
+and s.id = anp.persona
+and ap.progetto = p.id
+and ap.giorno = anp.giorno;
 
 
 
 --10--
-select distinct Persona.nome, Persona.cognome
-from Persona, AttivitaProgetto, Assenza
-where Persona.id = AttivitaProgetto.persona and Persona.id = Assenza.persona
-and AttivitaProgetto.giorno = Assenza.giorno;
+select distinct s.id, s.nome, s.cognome
+from Persona s, AttivitaProgetto ap, Assenza a
+where s.id = ap.persona 
+and s.id = a.persona
+and ap.giorno = a.giorno;
 
 
 --11--
-select distinct Persona.nome, Persona.cognome, AttivitaProgetto.giorno, Progetto.nome progetto,
-       Assenza.tipo causa_assenza, AttivitaProgetto.oreDurata
-from Persona, AttivitaProgetto, Assenza, Progetto
-where Persona.id = AttivitaProgetto.persona and Persona.id = Assenza.persona
-and AttivitaProgetto.progetto = Progetto.id
-and AttivitaProgetto.giorno = Assenza.giorno;
+select distinct s.nome, s.cognome, ap.giorno, p.nome,
+       a.tipo, ap.oreDurata
+from Persona s, AttivitaProgetto ap, Assenza a, Progetto p
+where s.id = ap.persona and s.id = a.persona
+and ap.progetto = p.id
+and ap.giorno = a.giorno;
 
 
 
 --12--
-select WP1.nome, Progetto1.nome progetto1, Progetto2.nome progetto2
-from WP WP1, Progetto Progetto1, WP WP2, Progetto Progetto2
-where WP1.progetto = Progetto1.id
-and WP1.nome = WP2.nome AND WP1.progetto <> WP2.progetto
-and WP2.progetto = Progetto2.id;
+select distinct WP1.nome, p1.nome, p2.nome
+from WP WP1, Progetto p1, WP WP2, Progetto p2
+where WP1.progetto = p1.id
+and WP1.nome = WP2.nome 
+and WP1.progetto <> WP2.progetto
+and WP2.progetto = p2.id;
